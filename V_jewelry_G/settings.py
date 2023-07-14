9943fbf2659270333f2249c264fcb76ba6cf8b6a
Vaidots <99909315+Vaidots@users.noreply.github.com>
2023-07-14 00:28:24 +0000

    Add bag context and delivery functionality

--- V_jewelry_G/settings.py
+++ V_jewelry_G/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -156,3 +157,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 100
+STANDARD_DELIVERY_PERCENTAGE = 10
