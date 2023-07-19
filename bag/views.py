e4e5871b1f2c83794dfe00a6e8ea3808c9790939
Vaidots <99909315+Vaidots@users.noreply.github.com>
2023-07-19 23:35:25 +0000

    Add option to choose the type of metal for products

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from products.models import Product
 
 # Create your views here.
 
@@ -9,16 +10,28 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    metal = None
+    if 'product_metal' in request.POST:
+        metal = request.POST['product_metal']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if metal:
+        if item_id in list(bag.keys()):
+            if metal in bag[item_id]['items_by_metal'].keys():
+                bag[item_id]['items_by_metal'][metal] += quantity
+            else:
+                bag[item_id]['items_by_metal'][metal] = quantity
+        else:
+            bag[item_id] = {'items_by_metal': {metal: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+    
