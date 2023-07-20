f6024f0864e8d11b6ed40c6d509d8096c8a9d10e
Vaidots <99909315+Vaidots@users.noreply.github.com>
2023-07-20 12:47:57 +0000

    Add +/- icons,adjust,remove bag options and calc subtotal in bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from products.models import Product
 
 # Create your views here.
@@ -35,3 +35,49 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
     
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    metal = None
+    if 'product_metal' in request.POST:
+        metal = request.POST['product_metal']
+    bag = request.session.get('bag', {})
+
+    if metal:
+        if quantity > 0:
+            bag[item_id]['items_by_metal'][metal] = quantity
+        else:
+            del bag[item_id]['items_by_metal'][metal]
+            if not bag[item_id]['items_by_metal']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        metal = None
+        if 'product_metal' in request.POST:
+            metal = request.POST['product_metal']
+        bag = request.session.get('bag', {})
+
+        if  metal:
+            del bag[item_id]['items_by_metal'][metal]
+            if not bag[item_id]['items_by_s']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
