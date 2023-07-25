c1ac0204b1632a8d32941ba191004e6b28823019
Vaidots <Vaidots30@gmail.com>
2023-07-25 16:38:44 +0000

    reinstalled psycopg2 to check if heroku deployment issue persists

--- V_jewelry_G/settings.py
+++ V_jewelry_G/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['projectportfolio5.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['projectportfolio5.heroku.com']
 
 
 # Application definition
