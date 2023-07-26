77b184eabc638e8854b203703a82368806752f29
Vaidots <Vaidots30@gmail.com>
2023-07-26 00:04:02 +0000

    Add error404/500 handlers

--- V_jewelry_G/urls.py
+++ V_jewelry_G/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404, handler500
 
 
 urlpatterns = [
@@ -30,3 +31,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = "V_jewelry_G.views.handler404"
+handler500 = "V_jewelry_G.views.handler500"
