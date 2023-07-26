02b9e7b7c00bb04732b1d7c628b256cb06e800db
Vaidots <Vaidots30@gmail.com>
2023-07-26 13:07:57 +0000

    Add footer and mailchimp newsletter

--- V_jewelry_G/settings.py
+++ V_jewelry_G/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['projectportfolio5-a694568fc098.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['projectportfolio5-a694568fc098.herokuapp.com', 'localhost', '8000-vaidots-project5-saf4gygyaqm.ws-eu102.gitpod.io']
 
 
 # Application definition
