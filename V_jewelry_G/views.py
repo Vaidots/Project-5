47978d2be81739ee2982ab1f4979cf945286deb8
Vaidots <Vaidots30@gmail.com>
2023-07-29 16:07:44 +0000

    temporarly removed all handlers except 404

--- V_jewelry_G/views.py
+++ V_jewelry_G/views.py
@@ -7,16 +7,16 @@ def handler404(request, exceptions):
     return render(request, "errors/404.html", status=404)
 
 
-def handler500(request):
-    """ Error Handler 500 - Server Error """
-    return render(request, "errors/500.html", status=500)
+# def handler500(request):
+#     """ Error Handler 500 - Server Error """
+#     return render(request, "errors/500.html", status=500)
 
 
-def handler403(request, exceptions):
-    """ Error Handler 403 - Server Error """
-    return render(request, "errors/403.html", status=403)
+# def handler403(request, exceptions):
+#     """ Error Handler 403 - Server Error """
+#     return render(request, "errors/403.html", status=403)
 
 
-def handler405(request):
-    """ Error Handler  - Server Error """
-    return render(request, "errors/405.html", status=405)
+# def handler405(request):
+#     """ Error Handler  - Server Error """
+#     return render(request, "errors/405.html", status=405)
