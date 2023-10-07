6008fc8339b16a96908a9d99948efa9880589b86
Vaidots <Vaidots30@gmail.com>
2023-10-07 13:54:52 +0000

    Add teamember path and added to navbar

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ContactMessage, name='contact'),
+    path('team-members/', views.team_member_list, name='team_member_list'),
+    path('team/<int:pk>/', views.team_member_detail, name='team_detail'),
 ]
